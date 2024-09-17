97a757a13b29f65af49a7377b5a5d60f79dc4daa
Albert Ciesla <albert383@gmail.com>
2024-09-17 16:46:26 +0200

    Button interaction and styles

---
+++ src/Button/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Button = () => {
+    return (
+        <button className="button" type="submit">
+            Oblicz
+        </button>
+    );
+};
+
+export default Button;
