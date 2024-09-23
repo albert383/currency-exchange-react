e63831236812a7943050ac37507f3fcd8c574cac
Albert Ciesla <albert383@gmail.com>
2024-09-23 19:52:41 +0200

    Convert CSS styles to styled-components in Form component

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import "./style.css";
 import currencies from "./currencies";
 import Result from "./Result";
 import Paragraph from "./Paragraph";
 import Clock from "./Clock/clock";
+import { FormWrapper, Fieldset, Legend, Label, InputField, ListItem, Button } from "./styled";
 
 const Form = () => {
     const [amount, setAmount] = useState(100);
     const [currency, setCurrency] = useState("EUR");
-    const [result, setResult] = useState("")
+    const [result, setResult] = useState("");
 
     const onSubmit = (event) => {
         event.preventDefault();
@@ -18,28 +18,27 @@ const Form = () => {
     };
 
     return (
-        <form onSubmit={onSubmit} className="form">
+        <FormWrapper onSubmit={onSubmit}>
             <Clock />
-            <fieldset className="form__fieldset">
-                <legend className="form__legend">KANTOR</legend>
+            <Fieldset>
+                <Legend>KANTOR</Legend>
                 <p>
-                    <label className="form__label">
+                    <Label>
                         *Kwota w (PLN):
-                        <input
+                        <InputField
                             type="number"
                             min="1"
                             step="any"
                             value={amount}
                             onChange={(e) => setAmount(e.target.value)}
-                            className="form__field"
                         />
-                    </label>
+                    </Label>
                 </p>
                 <p>Wybierz walutę wymiany:</p>
                 <ul>
                     {currencies.map((currencyOption) => (
-                        <li key={currencyOption.code} className="form__li">
-                            <label>
+                        <ListItem key={currencyOption.code}>
+                            <Label>
                                 {currencyOption.code}
                                 <input
                                     type="radio"
@@ -48,17 +47,15 @@ const Form = () => {
                                     checked={currency === currencyOption.code}
                                     onChange={(e) => setCurrency(e.target.value)}
                                 />
-                            </label>
-                        </li>
+                            </Label>
+                        </ListItem>
                     ))}
                 </ul>
-                <button className="button" type="submit">
-                    Oblicz
-                </button>
+                <Button type="submit">Oblicz</Button>
                 <Result result={result} />
                 <Paragraph title="* Pola oznaczone gwiazdką są wymagane" />
-            </fieldset>
-        </form>
+            </Fieldset>
+        </FormWrapper>
     );
 };
 
--- src/App/Form/styled.js
+++ src/App/Form/styled.js
@@ -1,25 +1,25 @@
-.form {
+import styled from "styled-components";
+
+export const FormWrapper = styled.form`
     border: dashed 5px rgba(0, 128, 128, 0.212);
     border-radius: 10px;
     background-color: rgb(188, 251, 230);
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     width: 100%;
     min-width: 650px;
-}
 
-@media (max-width: 767px) {
-    .form {
+    @media (max-width: 767px) {
         min-width: auto;
     }
-}
+`;
 
-.form__fieldset {
+export const Fieldset = styled.fieldset`
     border-radius: 5px;
     padding: 20px;
     margin: 20px;
-}
+`;
 
-.form__legend {
+export const Legend = styled.legend`
     text-align: left;
     border: 2px solid #ce330ca6;
     padding: 10px;
@@ -27,29 +27,29 @@
     background-color: teal;
     color: white;
     border-radius: 5px;
-}
+`;
 
-.form__label {
+export const Label = styled.label`
     margin-right: 10px;
     padding: 5px;
     max-width: 150px;
     width: 100%;
     padding-right: 20px;
-}
+`;
 
-.form__field {
+export const InputField = styled.input`
     padding: 10px;
     border: 1px solid #ccc;
     max-width: 500px;
     width: 95%;
     border-radius: 6px;
-}
+`;
 
-.form__li {
+export const ListItem = styled.li`
     list-style: none;
-}
+`;
 
-.button {
+export const Button = styled.button`
     width: 100%;
     border: solid transparent;
     background-color: teal;
@@ -57,15 +57,15 @@
     border-radius: 5px;
     color: aliceblue;
     padding: 10px;
-}
 
-.button:hover {
-    background-color: rgba(0, 128, 128, 0.829);
-    transition: 0.3s;
-    border: solid black;
-}
+    &:hover {
+        background-color: rgba(0, 128, 128, 0.829);
+        transition: 0.3s;
+        border: solid black;
+    }
 
-.button:active {
-    background-color: rgba(0, 128, 85, 0.678);
-    transition: 0.3s;
-}
+    &:active {
+        background-color: rgba(0, 128, 85, 0.678);
+        transition: 0.3s;
+    }
+`;
