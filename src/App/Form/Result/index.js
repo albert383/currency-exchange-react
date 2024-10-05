60d36fc0ed8f607fe9dcebc60acb9778f3602826
Albert Ciesla <albert383@gmail.com>
2024-10-05 09:51:28 +0200

    calculateResult function moved to Result component

--- src/App/Form/Result/index.js
+++ src/App/Form/Result/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import { Paragraph } from "./styled";
 
+export const calculateResult = (amount, selectedCurrency) => {
+    if (!selectedCurrency) {
+        return "Waluta nie jest dostępna.";
+    }
+    const calculatedValue = (amount * selectedCurrency.value).toFixed(2);
+    return `Otrzymasz ${calculatedValue} ${selectedCurrency.code}`;
+};
+
+
 const Result = ({ result }) => (
     <Paragraph>
         Otrzymasz: <strong>{result || "N/A"}</strong>
